9717a1f036bd1b618e77baf6e899cba836f91a86
ajiousama <kokoronophotograph@gmail.com>
2026-09-11 22:01:35 +0900

    Add rebuild channel model

---
+++ jun-iptv-rebuild/app/src/main/java/jp/jun/iptv/Channel.java
@@ -0,0 +1,14 @@
+package jp.jun.iptv;
+
+public final class Channel {
+    public String name = "";
+    public String tvgId = "";
+    public String tvgName = "";
+    public String logo = "";
+    public String group = "";
+    public String url = "";
+    public String nowTitle = "";
+    public String nextTitle = "";
+    public long nowStart = 0L;
+    public long nowStop = 0L;
+}
