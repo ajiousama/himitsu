ac10c3e0bce50af8cc43cacdf764b7f0fbfd33dd
ajiousama <kokoronophotograph@gmail.com>
2026-09-11 20:41:34 +0900

    Jun IPTV 0.2.1 metadata

--- jun-iptv/app/build.gradle.kts
+++ jun-iptv/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.jun.iptv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
     }
 
     compileOptions {
