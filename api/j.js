5b8251f5a4e813e57328a7b9eac55b398b9528a2
ajiousama <kokoronophotograph@gmail.com>
2026-09-22 13:37:46 +0900

    Keep Jun IPTV API as compatibility entrypoint

--- api/j.js
+++ api/j.js
@@ -1,6 +1,1 @@
-export default function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  res.statusCode = 302;
-  res.setHeader('Location', 'https://raw.githubusercontent.com/ajiousama/himitsu/jun-iptv-build/jun-iptv/releases/JunIPTV_TS401_latest.apk');
-  res.end();
-}
+export { default } from "../services/jun-iptv/redirect.mjs";
